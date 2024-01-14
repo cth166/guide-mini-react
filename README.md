d003031d83ae04c1b79a9aedf22fcbb1dcf542d4
cth166 <905546907@qq.com>
2024-01-14 19:58:34 +0800

    再操作一遍

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-# Day1
+# Day2
 
-- [x] 小步走。从直接操作`dom api`添加元素。慢慢抽象出虚拟节点的概念，最终递归地去调用render函数生成整个subTree。
-- [x] 通过vitest快照测试代替手动测试。测试createElement的数据结构更可靠。
-- [x] vite dev启动项目。碰见jsx语法，vite会自动转换成`React.createElement`。如果写`<App className='heiheihei' />`这种函数组件的结构是 type是一个函数类型，因此调用这个函数就可以拿到对应的vdom。这里先不考虑组件的props。
+- [x] 了解fiber架构的优点：
 
-今天状态：😊
+  1. 用来把挂载过程分成多个小块，一个渲染dom的动作对应一个fiber对象。
+  2. 任务链条中间中断了，可以简单地从中断处继续执行。因为各个任务的关系已经构建好了链表关系。
+  3. 在渲染真实的dom时，构建了当前fiber下所有children的关系。下一个任务指向第一个children，又在第一个children渲染成真实dom时，构建了此child下的children的关系。
 
-经验 + 10
+- [x] 任务链的查找顺序是先child，后subling，后叔叔节点。但是如果当前节点很深，只找父节点的subling就不太够用了。直白的做法就是一直递归往上找父节点的subling。不知道有没有更好的方法。
+
+  <div style="display:flex;height:300px">
+      <img src='https://s2.loli.net/2024/01/14/SmjtWuLc5ROiwd3.png' />
+      <img src='https://wechatapppro-1252524126.cdn.xiaoeknow.com/appewiejl9g3764/image/u_6482edb720c4d_DniGvx1NiY/lrdezlwz0g48.jpg?imageView2/2/w/1500/a/100|imageMogr2/ignore-error/1' />
+  </div>
+
+  
+
+  ![img](https://wechatapppro-1252524126.cdn.xiaoeknow.com/appewiejl9g3764/image/u_6482edb720c4d_DniGvx1NiY/lrdf1u0405kt.png?imageView2/2/w/1500/a/100|imageMogr2/ignore-error/1)
+
+今天状态：😅
+
+经验 + 15
--- core/React.js
+++ core/React.js
@@ -23,29 +23,76 @@ function createTextNode(text) {
 }
 
 function render(el, container) {
-  let dom
-  if (el.type === 'TEXT_ELEMENT') {
-    dom = document.createTextNode('')
-  } else if (typeof el.type === 'string') {
-    dom = document.createElement(el.type)
-  } else if (typeof el.type === 'function') {
-    const subTree = el.type(el.props)
-    console.log(subTree)
-    render(subTree, container)
-    return
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [el]
+    }
   }
+}
 
+function createDom(fiber) {
+  return fiber.type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(fiber.type)
+}
 
-  for (const [key, value] of Object.entries(el.props)) {
+function handleProps(dom, fiber) {
+  for (const [key, value] of Object.entries(fiber.props)) {
     if (key !== 'children') {
       dom[key] = value
     }
   }
-
-  el.props.children.forEach(child => render(child, dom))
-
-  container.appendChild(dom)
 }
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    const dom = fiber.dom = createDom(fiber)
+    fiber.parent.dom.append(dom)
+    handleProps(dom, fiber)
+  }
+
+  let prevFiber = null
+  const children = fiber.props.children
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      subling: null,
+      dom: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevFiber.subling = newFiber
+    }
+    prevFiber = newFiber
+  })
+
+  function traverseParent(fiber) {
+    if (!fiber) return null
+
+    return fiber.parent?.subling || traverseParent(fiber.parent)
+  }
+
+  // 返回下一个fiber的优先级。先child，后subing，后叔叔
+  return fiber.child || fiber.subling || fiber.parent.subling || traverseParent(fiber.parent)
+}
+
+let nextUnitOfWork = null
+function workLoop(deadline) {
+  let shouldYeild = false
+  while (!shouldYeild && nextUnitOfWork) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+
+    shouldYeild = deadline.timeRemaining() < 10
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
 
 const React = {
   createElement,
